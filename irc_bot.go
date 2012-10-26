307e35811d69b8f5bf16b9de4ccbc2a8685dba2f
chisun <iam.quadr@gmail.com>
2012-10-26 18:33:17 +0900

    봇 - 선곡기능 추가

--- irc_bot.go
+++ irc_bot.go
@@ -1,15 +1,86 @@
 package main
 
 import (
+	"crypto/tls"
+	"fmt"
 	irc "github.com/fluffle/goirc/client"
 	redis "github.com/garyburd/redigo/redis"
-//	"fmt"
+	"io/ioutil"
 	"log"
-//	"strings"
-	"crypto/tls"
+	"math/rand"
+	"strconv"
+	"strings"
+	"time"
 )
 
 var msg = make(chan string, 256)
+
+type MusicInfo struct {
+	title, artist string
+	bpm           int
+	diffculty     string
+	lv            int
+	notes         int
+}
+
+func (m *MusicInfo) String() string {
+	return fmt.Sprintf("%s - %s - %s - %d - Notes:%d - BPM:%d", m.title, m.artist, m.diffculty, m.lv, m.notes, m.bpm)
+}
+
+func atoi(s string) (i int) {
+	i, _ = strconv.Atoi(s)
+	return
+}
+
+var str_difficulty = []string{"BASIC", "ADVANCED", "EXTREME"}
+
+func parseMusicInfo(raw string) (info []*MusicInfo) {
+	s := strings.Split(raw, "\t")
+	if len(s) == 1 {
+		return
+	}
+
+	info = make([]*MusicInfo, 3)
+	for i := 0; i < 3; i++ {
+		info[i] = &MusicInfo{
+			title:     s[0],
+			artist:    s[1],
+			diffculty: str_difficulty[i],
+			bpm:       atoi(s[2]),
+			lv:        atoi(s[i+3]),
+			notes:     atoi(s[i+6]),
+		}
+	}
+	return
+}
+
+type MusicInfoContainer struct {
+	allSongs []*MusicInfo
+	lvSongs  map[int][]*MusicInfo
+}
+
+func makeMusicInfoContainer() *MusicInfoContainer {
+	c := &MusicInfoContainer{
+		lvSongs: make(map[int][]*MusicInfo),
+	}
+
+	content, err := ioutil.ReadFile("list.txt")
+	if err != nil {
+		log.Println("Init MusicInfo Failed")
+	}
+	lines := strings.Split(string(content), "\n")
+	for _, line := range lines {
+		infos := parseMusicInfo(line)
+		c.allSongs = append(c.allSongs, infos...)
+		for _, info := range infos {
+			l := c.lvSongs[info.lv]
+			c.lvSongs[info.lv] = append(l, info)
+		}
+	}
+	return c
+}
+
+var musicinfos = makeMusicInfoContainer()
 
 func readLog() {
 	c, err := redis.Dial("tcp", ":6379")
@@ -27,10 +98,28 @@ func readLog() {
 	}
 }
 
+func selectMusic(lv int, conn *irc.Conn) {
+	var info *MusicInfo
+	log.Println(lv)
+	switch {
+	case lv == 0:
+		info = musicinfos.allSongs[rand.Int31n(int32(len(musicinfos.allSongs)))]
+	case lv > 0 && lv < 11:
+		l := musicinfos.lvSongs[lv]
+		info = l[rand.Int31n(int32(len(l)))]
+	}
+	if info == nil {
+		conn.Privmsg("#jubeater", "잘못입력하셨습니다.")
+		return
+	}
+	conn.Privmsg("#jubeater", info.String())
+}
+
 func main() {
+	rand.Seed(time.Now().UnixNano())
 	c := irc.SimpleClient("Jubeat")
 	c.SSL = true
-	c.SSLConfig = &tls.Config{ InsecureSkipVerify: true }
+	c.SSLConfig = &tls.Config{InsecureSkipVerify: true}
 
 	disconnected := make(chan bool)
 	c.AddHandler("connected", func(conn *irc.Conn, line *irc.Line) {
@@ -45,7 +134,25 @@ func main() {
 					log.Println(m)
 					conn.Privmsg("#jubeater", m)
 				}
-			} ()
+			}()
+		}
+	})
+
+	c.AddHandler("PRIVMSG", func(conn *irc.Conn, line *irc.Line) {
+		if len(line.Args) == 2 && line.Args[0][0] == '#' {
+			cmds := strings.Fields(line.Args[1])
+			if len(cmds) > 0 && len(cmds[0]) > 2 && cmds[0][0] == '!' {
+				switch cmds[0][1:] {
+				case "선곡":
+					lv := 0
+					if len(cmds) > 1 {
+						lv = atoi(cmds[1])
+					}
+					selectMusic(lv, conn)
+				default:
+					conn.Privmsg("#jubeater", "잘못된 명령입니다.")
+				}
+			}
 		}
 	})
 
@@ -55,5 +162,5 @@ func main() {
 		log.Println("Connection error: ", err)
 		return
 	}
-	<- disconnected
+	<-disconnected
 }
